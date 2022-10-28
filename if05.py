76755fb7d42761f7bf77e960f5ef7fbfadd10c6a
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-28 23:03:57 +0500

    05

--- if05.py
+++ if05.py
@@ -1,3 +1,6 @@
+
+
+
 def main(n):
     """
     Find the largest digit of the five-digit number.
@@ -6,4 +9,22 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a = n%10
+    b = n%100//10
+    c = n%1000//100
+    d = n%10000//1000
+    e = n//10000
+    if a>b and a>c and a>d and a>e:
+        return a
+    if b>a and b>c and b>d and b>e:
+        return b
+    if c>a and c>b and c>d and c>e:
+        return c
+    if d>a and d>b and d>c and d>e:
+        return d
+    if e>a and e>b and e>c and e>d:
+        return e
+print(main(12345))
+       
+    
+   
