ddad104a80e8ed077129987252435961fe4efbf4
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-28 21:10:59 +0500

    1

--- if01.py
+++ if01.py
@@ -8,4 +8,10 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a>b and a>c:
+        return ('First number')
+    if b>a and b>c:
+        return ('Second number')
+    if c>a and c>b:
+        return ('Third number')
+print(main(7,9,3))
