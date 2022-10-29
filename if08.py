566d67876ff79c35b2dcb7f53d9b11f54be93802
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-29 23:29:34 +0500

    080

--- if08.py
+++ if08.py
@@ -14,4 +14,16 @@ def main(number):
     Returns:
         str: return answer.
     """
-    return
+    if number==1:
+        return 'Monday'
+    if number == 2:
+        return "Tuesday"
+    if number == 3:
+        return "Wednesday"
+    if number == 4:
+        return "Friday"
+    if number == 5:
+        return "Saturday"
+    if number ==6:
+        return "Sunday"
+print(main(5))
